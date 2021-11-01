8bd057cfb08dc156fe6ffb451f972993cfbd571d
Chase Olsen <skalolsen@gmail.com>
2021-11-01 11:25:57 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+const router = require('./routes/api')
+
+app.use(express.static(path.join(__dirname, '/angular-SCHOLARLY/static')))
+app.use('/api', router)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/angular-SCHOLARLY/static/index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Running app on localhost:${port}`)
+})
