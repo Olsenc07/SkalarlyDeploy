89771c69e790c7b55bf7e027dfc9668a1fa43ae5
Chase Olsen <skalolsen@gmail.com>
2021-11-23 23:27:35 -0500

    min height and width

--- angular-SCHOLARLY/src/app/app.component.ts
+++ angular-SCHOLARLY/src/app/app.component.ts
@@ -18,7 +18,8 @@ import { filter, map, tap } from 'rxjs/operators';
 })
 export class AppComponent implements OnInit {
 
-
+  minHeight = true;
+  minwidth = true;
 
 
   searchPop = false;
@@ -112,7 +113,15 @@ export class AppComponent implements OnInit {
       map((event: NavigationEnd) => event.url === '/' || event.url === '/messages')
     );
 
+    if (window.screen.height < 400){
+  this.minHeight = false;
+}
+
+    if (window.screen.width < 320){
+  this.minwidth = false;
+}
   }
+
 
   // Missing link to fix search icon movement i hope
   // searchIcon.addEventListener("click", activateSearch);
