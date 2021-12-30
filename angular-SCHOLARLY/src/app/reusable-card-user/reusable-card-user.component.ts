2ebf1336f5fe083d26599296000de08e0552f3f6
Chase Olsen <skalolsen@gmail.com>
2021-12-30 04:21:26 -0500

    moving onto other cards data display

--- angular-SCHOLARLY/src/app/reusable-card-user/reusable-card-user.component.ts
+++ angular-SCHOLARLY/src/app/reusable-card-user/reusable-card-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { StoreService } from '../services/store.service';
+import { StoreService, Profile } from '../services/store.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -7,13 +8,24 @@ import { StoreService } from '../services/store.service';
     templateUrl: './reusable-card-user.component.html',
     styleUrls: ['./reusable-card-user.component.scss'],
 })
-export class ReusableCardUserComponent {
+export class ReusableCardUserComponent implements OnInit {
+    storeProfiles: Profile[] = [];
+    profiles: Profile[] = [];
+    private profilesSub: Subscription;
     // sends request to data base to get mutual friends list, by clicking on shaking hands emoji
     profile = StoreService.profile$$;
     id = StoreService.userId$$;
 
 
-    constructor() { }
+    constructor(public storeService: StoreService) { }
+
+    ngOnInit() {
+        this.storeService.getProfiles();
+        this.profilesSub = this.storeService.getProfileUpdateListener()
+         .subscribe((profiles: Profile[]) => {
+             this.profiles = profiles;
+         });
+    }
 
 }
 
