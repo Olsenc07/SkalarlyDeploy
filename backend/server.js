89967fb34606c4b4fad0e41d11f45615fcfdc2a2
Chase Olsen <skalolsen@gmail.com>
2022-10-05 17:28:59 -0600

    pass

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ const privateVapidKey = '1zXzUpQkkPMygH5d00CmVwabGO6nzYUNEWXTRDwNBKI';
  const path = require('path');
  const mongoose = require('mongoose');
 
- const postRoutes = require('backend/routes/posts');
+//  inst being found when hosted
+ const postRoutes = require('/Users/chaseolsen/angular_scholarly_fs/backend/routes/posts');
  const userRoutes = require('/Users/chaseolsen/angular_scholarly_fs/backend/routes/user');
  const messageRoutes = require('/Users/chaseolsen/angular_scholarly_fs/backend/routes/messages')
  const followRoutes = require('/Users/chaseolsen/angular_scholarly_fs/backend/routes/follow')
