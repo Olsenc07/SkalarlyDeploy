51f4a4864004070d74295189f83dbf15f69dd952
Chase Olsen <skalolsen@gmail.com>
2022-10-13 08:27:52 -0600

    focus

--- angular-SCHOLARLY/src/app/app-routing.module.ts
+++ angular-SCHOLARLY/src/app/app-routing.module.ts
@@ -160,6 +160,7 @@ const routes: Routes = [
   // Directs to search page if user is logged in.
   // Directs to log in page if user isn't logged in.
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '/', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
