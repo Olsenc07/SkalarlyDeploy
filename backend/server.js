5cf1b5f8336e14bdc5b2a4cfb24590e9613529f8
Chase Olsen <skalolsen@gmail.com>
2022-10-14 08:16:42 -0600

    hollywood

--- backend/server.js
+++ backend/server.js
@@ -161,10 +161,10 @@ app.use("/api/follow", followRoutes);
 /**
  * Routes Definitions
  */
- app.use(express.static(path.join(__dirname + '/angular-SCHOLARLY/src/app/app.component.html')))
+ app.use(express.static('/angular-SCHOLARLY/src/app/app.component.html'))
 
  app.get("/", (req, res) => {
-   res.status(200).sendFile(path.join(__dirname, '/angular-SCHOLARLY/src/app/app.component.html'));
+   res.status(200).sendFile('/angular-SCHOLARLY/src/app/app.component.html');
 })
 app.get('*', (req, res) => {
     res.sendFile( '/app/angular-SCHOLARLY/src/index.html')
