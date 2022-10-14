8094119260706ca4c13ce502d756bff5359162d0
Chase Olsen <skalolsen@gmail.com>
2022-10-13 19:37:20 -0600

    bears

--- angular-SCHOLARLY/src/app/main-pages/main-pages.component.ts
+++ angular-SCHOLARLY/src/app/main-pages/main-pages.component.ts
@@ -59,35 +59,4 @@ export class MainPagesComponent implements OnInit {
   // Fills same way just for different reasons
   // Each button opens this page by there should be 4 different functions with each
   // sharin the open attendence comp, but each sends diff values/reasoning
-  openAttendanceSheet(): void {
-    this.bottomSheet.open(AttendanceComponent);
-  }
-}
-
-@Component({
-  selector: 'app-main-attendance',
-  templateUrl: './attendance.component.html',
-  styleUrls: ['./attendance.component.scss'],
-})
-export class AttendanceComponent {
-  constructor(private bottomSheetRef: MatBottomSheetRef<AttendanceComponent>) {}
-
-  openLink(event: MouseEvent): void {
-    this.bottomSheetRef.dismiss();
-    event.preventDefault();
-  }
-}
-
-@Component({
-  selector: 'app-main-tagged',
-  templateUrl: './tagged.component.html',
-  styleUrls: ['./attendance.component.scss'],
-})
-export class TaggedComponent {
-  constructor(private bottomSheetRef: MatBottomSheetRef<AttendanceComponent>) {}
-
-  openLink(event: MouseEvent): void {
-    this.bottomSheetRef.dismiss();
-    event.preventDefault();
-  }
 }
