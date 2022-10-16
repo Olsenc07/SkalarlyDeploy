3cc740297ac549aa8f2a9c076fd491a5e2703f36
Chase Olsen <skalolsen@gmail.com>
2022-10-16 09:58:45 -0600

    ndsvonsodnv

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -15,7 +15,7 @@ const UserInfo = require('/app/backend/models/userInfo');
 // mail sender details
 var transporter = nodemailer.createTransport({
     name: 'www.skalarly.com',
-    host: 'smtp.skalarly.com',
+    host: 'smtp.domain.com',
     port: 587,
     auth: {
         // gmail just change to gmail email and service to gmail
