d0ff8c074b09659f7b317c1de770a0d423d062ac
Chase Olsen <skalolsen@gmail.com>
2022-10-18 15:29:03 -0600

    setting file

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -355,7 +355,7 @@ router.post("/showCases",
     (req, res) => {
     // const url = req.protocol + '://' + req.get('host');
 // if (req.file){
-    const showCaseImg =  cloudinary.v2.uploader.upload(req.file.path)
+    const showCaseImg =  cloudinary.v2.uploader.upload(req.file.path, {folder:'ShowCase'})
     var ShowCase = new showCase({
         // ShowCasePath: url + '/showCase/' + req.file.filename,
         ShowCasePath: showCaseImg.secure_url,
