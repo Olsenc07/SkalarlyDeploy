b1a9d1539d23103b7b8b4256c319d580a46009b9
Chase Olsen <skalolsen@gmail.com>
2022-10-19 16:19:53 -0600

    gas

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -340,17 +340,10 @@ router.post("/showCases",
     (req, res) => {
         let streamUpload = (req) => {
         return new Promise((resolve, reject) => {
-            let stream = cloudinary.uploader.upload_stream(
-              (error, result) => {
-                if (result) {
-                  resolve(result);
-                } else {
-                  reject(error);
-                }
-              }
-            );
-            streamifier.createReadStream(req.file.buffer).pipe(stream),
-            {folder: 'ShowCase' }
+            let stream = cloudinary.uploader.upload_stream(streamifier.
+             
+                //     {folder: 'ShowCase' });
+            streamifier.createReadStream(req.file.buffer),{folder: 'ShowCase' }).pipe(stream)
         });
     };
 console.log('check this out', streamUpload)
