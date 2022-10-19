84ceeb9f5540a8977c7d75f177ec65aeb75ace32
Chase Olsen <skalolsen@gmail.com>
2022-10-19 11:07:21 -0600

    hy

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -45,7 +45,7 @@ const storage  = multer.diskStorage({
     
 });
 
-const storage_2 = multer.memoryStorage();
+const storage_2 = multer();
 // .({
 //     // destination: (req, file, cb) => {
 //     //     const isValid = MIME_TYPE_MAP[file.mimetype];
@@ -363,7 +363,7 @@ router.post("/showCases",
 if(req.file){console.log(req.file)}
 if(req.file.buffer){req.body,console.log('big tits_')}
 if(req.body.Creator){req.body,console.log('big tatters')}
-    const showCaseImg =  cloudinary.uploader.upload(req.file, 
+    const showCaseImg =  cloudinary.uploader.upload(req.file.buffer, 
         {folder: 'ShowCase' });
     var ShowCase = new showCase({
         // ShowCasePath: url + '/showCase/' + req.file.filename,
