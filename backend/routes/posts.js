386d787f31c9b14e743de99ad2ab220c80e90ce3
Chase Olsen <skalolsen@gmail.com>
2022-10-19 11:27:49 -0600

    ladies

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -330,7 +330,7 @@ router.get("/showCasesPersonal", async(req, res, next) => {
         });
     });
 });
-const show = multer()
+const show = multer.memoryStorage()
 // showCase additions
 router.post("/showCases", 
     checkAuth,
@@ -339,8 +339,8 @@ router.post("/showCases",
     // const url = req.protocol + '://' + req.get('host');
 if(req.file){console.log(req.file)}
 if(req.file.buffer){req.body,console.log('big tits_')}
-if(req.body.Creator){req.body,console.log('big tatters')}
-    const showCaseImg =  cloudinary.uploader.upload(req.file, 
+if(req.body.showCase){req.body,console.log('big tatters')}
+    const showCaseImg =  cloudinary.uploader.upload(req.file.buffer, 
         {folder: 'ShowCase' });
     var ShowCase = new showCase({
         // ShowCasePath: url + '/showCase/' + req.file.filename,
