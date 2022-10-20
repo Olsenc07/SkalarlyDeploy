d8862b34c3fe6ddfca8fb071d1c6cbf07fe9bdb2
Chase Olsen <skalolsen@gmail.com>
2022-10-20 09:46:42 -0600

    refer

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -344,7 +344,7 @@ router.post("/showCases",
     (req, res) => {
         let streamUpload = (req) => {
         return new Promise((resolve, reject) => {
-        let stream = cloudinary.uploader.upload_stream({folder: 'ShowCase'}, req.file,
+        let stream = cloudinary.uploader.upload({folder: 'ShowCase'}, req.file,
             (error, result) => {
                 if (result) {
                   resolve(result);
