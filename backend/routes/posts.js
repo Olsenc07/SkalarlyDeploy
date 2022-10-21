c4083cdd009990f8cc5b4959ac733ad049a8812e
Chase Olsen <skalolsen@gmail.com>
2022-10-20 20:41:43 -0600

    cousion

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -353,7 +353,7 @@ router.post("/showCases",
         // console.log(cloudinary.config());
         console.log('chase', req.file)
 
-        const upload = await cloudinary.uploader.upload('https://api.cloudinary.com/v1_1/${skalarly}/upload',req.file, {
+        const upload = await cloudinary.uploader.upload(req.file.destination, {
            folder:'ShowCase'
         })
         .then(result=>console.log(result));;
