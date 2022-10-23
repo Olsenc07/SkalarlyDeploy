2ceb1d6f9eda8737b624afdfde866220e5b08896
Chase Olsen <skalolsen@gmail.com>
2022-10-23 09:39:10 -0600

    no more time out

--- angular-SCHOLARLY/src/app/services/auth.service.ts
+++ angular-SCHOLARLY/src/app/services/auth.service.ts
@@ -973,6 +973,8 @@ export class AuthService {
             CodePursuing10: '',
             CodePursuing11: '',
             CodePursuing12: '',
+            CodePursuing13: '',
+            CodePursuing14: '',
             CodeCompleted31: '',
             CodeCompleted32: '',
             CodeCompleted33: '',
@@ -999,8 +1001,6 @@ export class AuthService {
             CodeCompleted23: '',
             CodeCompleted24: '',
             CodeCompleted25: '',
-            CodePursuing13: '',
-            CodePursuing14: '',
           };
           this.router.navigate(['/profile']);
           this.snackBar.open('Profile edited!', 'Nice!', {
@@ -1623,8 +1623,6 @@ export class AuthService {
               CodePursuing14: info.CodePursuing14,
               ProfilePicPath: info.ProfilePicPath,
               // ShowCasePath: info.ShowCasePath,
-              Followers: info.followers,
-              Followings: info.followings,
               Creator: info.Creator,
             };
           });
@@ -1715,8 +1713,6 @@ export class AuthService {
               CodePursuing14: info.CodePursuing14,
               ProfilePicPath: info.ProfilePicPath,
               // ShowCasePath: info.ShowCasePath,
-              Followers: info.followers,
-              Followings: info.followings,
               Creator: info.Creator,
             };
           });
@@ -1808,8 +1804,6 @@ export class AuthService {
 
               ProfilePicPath: info.ProfilePicPath,
               // ShowCasePath: info.ShowCasePath,
-              Followers: info.followers,
-              Followings: info.followings,
               Creator: info.Creator,
             };
           });
@@ -1902,8 +1896,6 @@ export class AuthService {
 
               ProfilePicPath: info.ProfilePicPath,
               // ShowCasePath: info.ShowCasePath,
-              Followers: info.followers,
-              Followings: info.followings,
               Creator: info.Creator,
             };
           });
@@ -1992,8 +1984,6 @@ export class AuthService {
 
               ProfilePicPath: info.ProfilePicPath,
               // ShowCasePath: info.ShowCasePath,
-              Followers: info.followers,
-              Followings: info.followings,
               Creator: info.Creator,
             };
           });
@@ -2008,8 +1998,9 @@ export class AuthService {
   // Login
   login(email: string, password: string): any {
     const authData: AuthData = { email, password };
+    // expiresIn: number;
     this.http
-      .post<{ token: string; expiresIn: number; userId: string }>(
+      .post<{ token: string; userId: string }>(
         'http://www.skalarly.com/api/user/login',
         authData
       )
@@ -2022,19 +2013,20 @@ export class AuthService {
             this.snackBar.open('Welcome Fellow Skalar!', 'Thanks! ', {
               duration: 3000,
             });
-            const expiresInDuration = response.expiresIn;
-            this.setAuthTimer(expiresInDuration);
+            // const expiresInDuration = response.expiresIn;
+            // this.setAuthTimer(expiresInDuration);
             this.isAuthenticated = true;
             this.userId = response.userId;
             this.authStatusListener.next(true);
-            const now = new Date();
-            const expirationDate = new Date(
-              now.getTime() + expiresInDuration * 1000
-            );
-
-            this.saveAuthData(token, expirationDate, this.userId);
-            console.log(expirationDate);
-            console.log(this.token);
+            // const now = new Date();
+            // const expirationDate = new Date(
+            //   now.getTime() + expiresInDuration * 1000
+            // );
+            //  expirationDate,
+            //
+            this.saveAuthData(token, this.userId);
+            // console.log(expirationDate);
+            // console.log(this.token);
           }
         },
         error: (error) => {
@@ -2051,7 +2043,7 @@ export class AuthService {
     const authData = { emailV, passwordV };
     this.http
       // expiresIn: number;
-      .post<{ token: string; expiresIn: number; userId: string }>(
+      .post<{ token: string; userId: string }>(
         'http://www.skalarly.com/api/user/login1',
         authData
       )
@@ -2059,16 +2051,16 @@ export class AuthService {
         next: (response) => {
           this.token = response.token;
           this.isAuthenticated = true;
-          const expiresInDuration = response.expiresIn;
-          this.setAuthTimer(expiresInDuration);
+          // const expiresInDuration = response.expiresIn;
+          // this.setAuthTimer(expiresInDuration);
           this.userId = response.userId;
           this.authStatusListener.next(true);
-          const now = new Date();
-          const expirationDate = new Date(
-            now.getTime() + expiresInDuration * 1000
-          );
-
-          this.saveAuthData(this.token, expirationDate, this.userId);
+          // const now = new Date();
+          // const expirationDate = new Date(
+          //   now.getTime() + expiresInDuration * 1000
+          // );
+          // expirationDate,
+          this.saveAuthData(this.token, this.userId);
         },
         error: (error) => {
           this.authStatusListener.next(false);
@@ -2118,30 +2110,33 @@ export class AuthService {
 
   private saveAuthData(
     token: string,
-    expirationDate: Date,
+    // expirationDate: Date,
     userId: string
   ): any {
     localStorage.setItem('token', token);
-    localStorage.setItem('expiration', expirationDate.toISOString());
+    // localStorage.setItem('expiration', expirationDate.toISOString());
     localStorage.setItem('userId', userId);
   }
 
   private clearAuthData(): any {
     localStorage.removeItem('token');
-    localStorage.removeItem('expiration');
+    // localStorage.removeItem('expiration');
     localStorage.removeItem('userId');
   }
 
   private getAuthData(): any {
     const token = localStorage.getItem('token');
-    const expirationDate = localStorage.getItem('expiration');
+    // const expirationDate = localStorage.getItem('expiration');
     const userId = localStorage.getItem('userId');
-    if (!token || !expirationDate) {
+    // if (!token || !expirationDate) {
+    //   return;
+    // }
+    if (!token) {
       return;
     }
     return {
       token,
-      expirationDate: new Date(expirationDate),
+      // expirationDate: new Date(expirationDate),
       userId,
     };
   }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -700,7 +700,7 @@ router.post("/login1", verifyEmailV, async(req, res, next) => {
                     );
                     res.status(200).json({
                         token: token,
-                        expiresIn: 3600,
+                        // expiresIn: 3600,
                         userId: fetchedUser._id
                     });
                 })
