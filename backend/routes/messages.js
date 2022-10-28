b16940148fc32cd8ec617aad10688c795cebcd04
Chase Olsen <skalolsen@gmail.com>
2022-10-28 17:29:57 -0600

    fix

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -98,7 +98,7 @@ if(req.query.username === req.query.userId ){
 
 // listen for chat msg sending
 
-router.get('/OnetoOneSend/:userId', async(req,res) => {
+router.get('/OnetoOneSend', async(req,res) => {
     console.log('made it')
     var io = req.app.get('socketio');
     
