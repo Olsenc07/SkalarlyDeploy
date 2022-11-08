e76dc1e786981e0313ad0106b40fe3b4453368f1
Chase Olsen <skalolsen@gmail.com>
2022-11-07 18:32:30 -0700

    boo yeah

--- backend/server.js
+++ backend/server.js
@@ -65,13 +65,13 @@ app.post('/subscribe', (req, res) => {
 var options = {
     key: fs.readFileSync(path.join(__dirname, 'skalarly.com_key.txt')),
     cert: fs.readFileSync(path.join(__dirname, 'skalarly.com.crt')),
-    ca: fs.readFileSync(path.join(__dirname, 'skalarly.com.ca-bundle'))
+    // ca: fs.readFileSync(path.join(__dirname, 'skalarly.com.ca-bundle'))
    };
    
  const server = https.createServer(options, app)
  const port = process.env.PORT || 3000;
-//  const port = process.env.PORT || 5000
- const router = express.Router();
+
+
 
 
 //  Socket.io 
