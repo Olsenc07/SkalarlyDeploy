5ca2aff6f4f21426d0f14feed73dcd76f0fcfda9
Chase Olsen <skalolsen@gmail.com>
2022-11-13 16:09:36 -0700

    hey

--- backend/server.js
+++ backend/server.js
@@ -186,7 +186,7 @@ app.use("/api/follow", followRoutes);
  function requireHTTPS(req, res, next) {
     // The 'x-forwarded-proto' check is for Heroku
     if (
-      !req.secure &&
+    //   !req.secure &&
       req.get("x-forwarded-proto") !== "https"
     ) {
       return res.redirect("https://" + req.get("host") + req.url);
