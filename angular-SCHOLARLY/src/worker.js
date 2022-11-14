c662a64d7f6d5a2fd74682bfaeadd8eb863b8e00
Chase Olsen <skalolsen@gmail.com>
2022-11-14 10:31:03 -0700

    notifs

--- angular-SCHOLARLY/src/worker.js
+++ angular-SCHOLARLY/src/worker.js
@@ -13,17 +13,13 @@ self.addEventListener('install', event => {
   });
 
 
-//   if('Notification' in window){
-//     console.log('money')
-// self.addEventListener('click', askForNotificationPermission);
-//   }
-// self.addEventListener('push', e => {
-// const data = e.data.json();
-// console.log('Push Recieved...');
-// registration.showNotification(data.title, {
-//     body:'Notified by Skalarly',
-//     icon: '/Users/chaseolsen/angular_scholarly_fs/angular-SCHOLARLY/src/assets/Pics/Skalarly jpeg 2 (hat & logo).png'
-// })
-// });
-
-// module.exports = router;
+self.addEventListener('notificationclick', function(event) {
+  var notification = event.notification;
+  var action = event.action;
+  console.log(notification);
+  if (action === 'confirm'){
+    notification.close()
+  }else{
+    console.log(action);
+  }
+})
