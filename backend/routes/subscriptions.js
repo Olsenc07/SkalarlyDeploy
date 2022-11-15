49ffa6abee33abbd85d9f5b5f058e3da3978bad2
Chase Olsen <skalolsen@gmail.com>
2022-11-14 18:54:17 -0700

    find rpute

--- backend/routes/subscriptions.js
+++ backend/routes/subscriptions.js
@@ -1,6 +1,7 @@
 const Subscription = require('/app/backend/models/subscription');
 const User = require('/app/backend/models/user');
 const express = require('express');
+
 const UserInfo = require('/app/backend/models/userInfo');
 const router = express.Router();
 const webpush = require('web-push');
@@ -10,7 +11,7 @@ privateVapidKey = process.env.vapidPrivate
 
 
 
-router.post("/follow", (req, res, next) => {
+router.post("/api/subscribe/follow", (req, res, next) => {
     console.log('route made it')
     // var subscription = new Subscription({
     //     userId: req.userData.userId,
