01305d9658b14dd15aaa77050de36b597d30224c
Chase Olsen <skalolsen@gmail.com>
2022-11-14 21:00:33 -0700

    changes

--- backend/routes/subscriptions.js
+++ backend/routes/subscriptions.js
@@ -13,33 +13,35 @@ privateVapidKey = process.env.vapidPrivate
 
 router.post("/follow", (req, res, next) => {
     console.log('route made it',req.body)
-   
-    // if (!isValidSaveRequest(req, res)) {
-    //     return;
-    //   }
-    //   var subscription = new Subscription({
-    //     userId: req.userData.userId,
-    //     endpoint: reg,
 
+      var subscription = new Subscription({
+        userId: req.userData.userId,
+        endpoint: req.body.endpoint,
+        keys: {
+            p256dh: req.body.keys,
+            auth: req.body.auth,
+          },
+      
 
-    // })
-    //     .then(function (subscriptionId) {
-    //       res.setHeader('Content-Type', 'application/json');
-    //       res.send(JSON.stringify({data: {success: true}}));
-    //     })
-    //     .catch(function (err) {
-    //       res.status(500);
-    //       res.setHeader('Content-Type', 'application/json');
-    //       res.send(
-    //         JSON.stringify({
-    //           error: {
-    //             id: 'unable-to-save-subscription',
-    //             message:
-    //               'The subscription was received but we were unable to save it to our database.',
-    //           },
-    //         }),
-    //       );
-    //     });
+    })
+        .then(function (subscriptionId) {
+         subscription.save()
+          res.setHeader('Content-Type', 'application/json');
+          res.send(JSON.stringify({data: {success: true}}));
+        })
+        .catch(function (err) {
+          res.status(500);
+          res.setHeader('Content-Type', 'application/json');
+          res.send(
+            JSON.stringify({
+              error: {
+                id: 'unable-to-save-subscription',
+                message:
+                  'The subscription was received but we were unable to save it to our database.',
+              },
+            }),
+          );
+        });
 
 })
 
