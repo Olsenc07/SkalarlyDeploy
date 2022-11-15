ad54d61b1cc21dfd068588a2f870cb186818e48d
Chase Olsen <skalolsen@gmail.com>
2022-11-15 15:41:26 -0700

    shmuck

--- angular-SCHOLARLY/src/worker.js
+++ angular-SCHOLARLY/src/worker.js
@@ -32,12 +32,7 @@ self.addEventListener('push', (event) => {
   console.log('pushing notifications',event);
   console.log('pushinging notifications',event.data);
 
-  ServiceWorkerRegistration.showNotification(
-    'New Follower',
-      {
-            body: "Push notification from section.io", //the body of the push notification
-            image: "/angular-SCHOLARLY/src/assets/Pics/Skalarly jpeg 2 (hat & logo).png",
-            icon: "/angular-SCHOLARLY/src/assets/Pics/Skalarly 1.jpeg" // icon 
-        })
+  const promiseChain = self.registration.showNotification('Hello, World.');
+  event.waitUntil(promiseChain);
 });
 
