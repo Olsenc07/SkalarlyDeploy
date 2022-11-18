f427990d91bf78e27e74bb23bcb674a1a42e1fb6
Chase Olsen <skalolsen@gmail.com>
2022-11-18 14:07:14 -0700

    blow in the hole

--- backend/routes/subscriptions.js
+++ backend/routes/subscriptions.js
@@ -50,7 +50,7 @@ router.post("/new", (req, res, next) => {
       webpush.sendNotification(pushSubscription, JSON.stringify({
           title: 'Successful Connection',
           content: `${user.username} will be notified when other users interact with you.`,
-          openUrl: '/friends-activity'
+          // openUrl: '/friends-activity'
       }), options)
       .then((_) => {
         console.log( 'SENT!');
