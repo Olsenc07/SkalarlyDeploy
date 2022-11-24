d6b820b7e5548d88b8da1214119902670fd089b7
Chase Olsen <skalolsen@gmail.com>
2022-11-23 18:30:27 -0700

    fox

--- angular-SCHOLARLY/src/app/edit-profile/edit-profile.component.ts
+++ angular-SCHOLARLY/src/app/edit-profile/edit-profile.component.ts
@@ -54,7 +54,7 @@ export class EditProfileComponent implements OnInit {
 
   userId: string;
 
-  infos: AuthDataInfo[] = [];
+  infos: AuthDataInfo[];
   private infosSub: Subscription;
   // Showcase
   showCasePreview: any = '';
