91ab8c02c33531c23fed70b65262ef0c21532cdb
Chase Olsen <skalolsen@gmail.com>
2022-12-03 09:27:03 -0700

    started

--- angular-SCHOLARLY/src/app/retrieve-password/retrieve-password.component.ts
+++ angular-SCHOLARLY/src/app/retrieve-password/retrieve-password.component.ts
@@ -18,6 +18,7 @@ import { PostsService } from '../services/posts.service';
 })
 export class RetrievePasswordComponent implements OnInit {
   userId: string;
+  notif: string;
   isLoading = false;
   visible = true;
   passwordDel: FormControl = new FormControl('', Validators.minLength(8));
@@ -48,6 +49,8 @@ export class RetrievePasswordComponent implements OnInit {
 
   ngOnInit(): void {
     this.userId = this.authService.getUserId();
+    this.notif = this.postsService.getNotifId();
+    console.log('During the day', this.notif);
   }
 
   // Reset notifications
