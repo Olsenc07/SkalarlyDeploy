9e39766121f68e46890d84bb8954b6f340092861
Chase Olsen <skalolsen@gmail.com>
2022-12-07 11:45:19 -0700

    algor

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1445,7 +1445,7 @@ router.put("/infoEdPic", checkAuth,
                 if(req.file){
                     await cloudinary.uploader.upload(req.file.path, {
                         transformation: [
-                            { gravity: "face", crop: "crop"},
+                            { gravity: "face", width: 55, height: 55, crop: "fill"},
                         ],
                     folder:'ProfilePics'
                  })
