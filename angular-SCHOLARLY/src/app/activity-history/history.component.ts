0286b53824e2f64eaf3e5cb1675b6fe50f9e72c9
Chase Olsen <skalolsen@gmail.com>
2023-01-07 00:13:14 -0700

    mint

--- angular-SCHOLARLY/src/app/activity-history/history.component.ts
+++ angular-SCHOLARLY/src/app/activity-history/history.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'activity-history',
   templateUrl: './history.component.html',
-  styleUrls: ['./history.component.scss'],
+  styleUrls: ['../friends-activity/friends-activity.component.scss'],
 })
 export class ActivityHistoryComponent implements OnInit {
   ngOnInit() {}
