42af05170f6d21aa0ed6223c304d22e32f5ad17b
Chase Olsen <skalolsen@gmail.com>
2023-01-11 00:41:27 -0700

    nice

--- angular-SCHOLARLY/src/app/main-pages/main-pages.component.ts
+++ angular-SCHOLARLY/src/app/main-pages/main-pages.component.ts
@@ -8,6 +8,7 @@ import {
 } from '@angular/material/bottom-sheet';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { CommentsService } from '../services/comments.service';
 
 @Component({
   selector: 'app-main-pages',
@@ -79,17 +80,45 @@ export class SinglePageComponent implements OnInit {
 })
 export class SinglePageTemplateComponent implements OnInit {
   isLoading = false;
+  userId: string;
   posts: Post[] = [];
   postId: string;
+  comment: FormControl = new FormControl('');
+  comments: string[] = [];
+  // number of comments that load
+  private commentsSub: Subscription;
+  timeHourInitial = new Date().getHours();
+  timeHour = this.testNum(this.timeHourInitial);
+  timeMinute = new Date().getMinutes();
+  text = this.timeHourInitial >= 12 ? 'pm' : 'am';
+  timeMinuteText = this.timeMinute < 10 ? '0' : '';
+  dateDay = new Date().getDate();
+  dateMonth = new Date().getMonth();
+  dateMonthName = this.testMonth(this.dateMonth);
+  time =
+    this.dateMonthName +
+    '\xa0' +
+    this.dateDay +
+    '\xa0' +
+    this.timeHour +
+    ':' +
+    this.timeMinuteText +
+    this.timeMinute +
+    '\xa0' +
+    this.text;
   private postsSub: Subscription;
 
   constructor(
     private route: ActivatedRoute,
     public postService: PostService,
-    private router: Router
+    private authService: AuthService,
+    private router: Router,
+    private commentsService: CommentsService
   ) {}
 
   ngOnInit(): void {
+    this.userId = this.authService.getUserId();
+
     this.route.queryParams.subscribe((params) => {
       console.log('params single page', params);
       this.postId = params?.postId;
@@ -104,6 +133,87 @@ export class SinglePageTemplateComponent implements OnInit {
         });
     });
   }
+  // Am Pm instead of 24hr clock
+  testNum(timeHourInitial: any): number {
+    if (timeHourInitial > 12) {
+      if (timeHourInitial === 13) {
+        return 1;
+      }
+      if (timeHourInitial === 14) {
+        return 2;
+      }
+      if (timeHourInitial === 15) {
+        return 3;
+      }
+      if (timeHourInitial === 16) {
+        return 4;
+      }
+      if (timeHourInitial === 17) {
+        return 5;
+      }
+      if (timeHourInitial === 18) {
+        return 6;
+      }
+      if (timeHourInitial === 19) {
+        return 7;
+      }
+      if (timeHourInitial === 20) {
+        return 8;
+      }
+      if (timeHourInitial === 21) {
+        return 9;
+      }
+      if (timeHourInitial === 22) {
+        return 10;
+      }
+      if (timeHourInitial === 23) {
+        return 11;
+      }
+      if (timeHourInitial === 24) {
+        return 12;
+      }
+    } else {
+      return timeHourInitial;
+    }
+  }
+  testMonth(dateMonth: any): string {
+    if (dateMonth === 0) {
+      return 'Jan';
+    }
+    if (dateMonth === 1) {
+      return 'Feb';
+    }
+    if (dateMonth === 2) {
+      return 'Mar';
+    }
+    if (dateMonth === 3) {
+      return 'Apr';
+    }
+    if (dateMonth === 4) {
+      return 'May';
+    }
+    if (dateMonth === 5) {
+      return 'June';
+    }
+    if (dateMonth === 6) {
+      return 'July';
+    }
+    if (dateMonth === 7) {
+      return 'Aug';
+    }
+    if (dateMonth === 8) {
+      return 'Sept';
+    }
+    if (dateMonth === 9) {
+      return 'Oct';
+    }
+    if (dateMonth === 10) {
+      return 'Nov';
+    }
+    if (dateMonth === 11) {
+      return 'Dec';
+    }
+  }
   navigateToPage(infoUser: string): any {
     // const ID = (document.getElementById('userName') as HTMLInputElement).value;
     this.router.navigate(['/skalars/:'], { queryParams: { id: infoUser } });
@@ -112,5 +222,31 @@ export class SinglePageTemplateComponent implements OnInit {
   navigateToMainPage(value: string): void {
     this.router.navigate(['/main/:'], { queryParams: { category: value } });
     console.log('hey chaz mataz', value);
+  }
+  onDeleteComment(commentId: string): any {
+    this.commentsService.deleteComment(commentId);
+    console.log('chaz whats up', commentId);
+  }
+  CommentTrigger(postId): void {
+    if (this.comment.value) {
+      this.commentsService.createComment(
+        this.comment.value,
+        this.userId,
+        this.time,
+        postId
+      );
+      this.comment.setValue('');
+      console.log('onComment', postId);
+    }
+  }
+
+  loadComments(postId: string): void {
+    console.log('hey logic fade away', postId);
+    this.commentsService.getComments(postId);
+    this.commentsSub = this.commentsService
+      .getMessagesUpdateListener()
+      .subscribe((comments: string[]) => {
+        this.comments = comments;
+      });
   }
 }
