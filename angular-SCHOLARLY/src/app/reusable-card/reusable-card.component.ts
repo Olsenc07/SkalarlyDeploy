c6f20e5f85a4e6c3af2e714209806361da35a4f2
Chase Olsen <skalolsen@gmail.com>
2023-01-23 20:22:03 -0700

    lovely

--- angular-SCHOLARLY/src/app/reusable-card/reusable-card.component.ts
+++ angular-SCHOLARLY/src/app/reusable-card/reusable-card.component.ts
@@ -1864,12 +1864,12 @@ export class CardFriendsComponent implements OnInit {
   }
 
   getPostsTrendingNumber(OriginalPostId: string, postId: string): any {
-    console.log('Hey babe I miss you more', postId);
-    console.log('Hey babe I miss you more', OriginalPostId);
-    if (OriginalPostId === undefined) {
+    console.log('Hey babe I miss you more', postId.length);
+    console.log('Hey babe I miss you ', OriginalPostId.length);
+    if (OriginalPostId.length === 0) {
       this.postService.getPostsTrendingNumber(postId);
       this.valueChosen = postId;
-      console.log('logic', this.valueChosen);
+      console.log('logic1', this.valueChosen);
     } else {
       this.postService.getPostsTrendingNumber(OriginalPostId);
       this.valueChosen = OriginalPostId;
