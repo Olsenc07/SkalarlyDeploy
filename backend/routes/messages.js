458cd449bedb46fee7216cd239b811b45a8466ca
Chase Olsen <skalolsen@gmail.com>
2023-02-10 13:22:30 -0700

    tester

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -104,7 +104,7 @@ for(let i in nonyaOnce){
 {otherUser: user.username},
 {username: nonyaOnce[i]}
     ]
-    }).sort({time:-1})
+    }).sort({_id:-1})
       
       .then(finalDocs => {
         console.log('finaldocs', finalDocs)
