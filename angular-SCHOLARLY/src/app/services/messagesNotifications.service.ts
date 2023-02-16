08023b391fb24b72f45e0cf84fe8c56a75a0f2af
Chase Olsen <skalolsen@gmail.com>
2023-02-15 22:01:26 -0700

    avenger

--- angular-SCHOLARLY/src/app/services/messagesNotifications.service.ts
+++ angular-SCHOLARLY/src/app/services/messagesNotifications.service.ts
@@ -91,12 +91,13 @@ export class MessageNotificationService {
         })
       )
       .subscribe((transformedMessage) => {
+        console.log('shark', transformedMessage);
         this.messagesNotif = transformedMessage;
         console.log('deep end', this.messagesNotif);
-        if (this.messagesNotif.length > 0) {
-          console.log('whast the length', this.messagesNotif.length);
-          this.messgesInfoUpdatedNotifs.next([...this.messagesNotif]);
-        }
+        // if (this.messagesNotif.length > 0) {
+        console.log('whast the length', this.messagesNotif.length);
+        this.messgesInfoUpdatedNotifs.next([...this.messagesNotif]);
+        // }
       });
   }
 
