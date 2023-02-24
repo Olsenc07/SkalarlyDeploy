b3ea1a5785e3ea519c3e3dc3cd96149ce5ca55bd
Chase Olsen <skalolsen@gmail.com>
2023-02-24 14:42:32 -0700

    sweet

--- angular-SCHOLARLY/src/app/error/error.component.ts
+++ angular-SCHOLARLY/src/app/error/error.component.ts
@@ -2,11 +2,13 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
-    templateUrl: './error.component.html',
-    styleUrls: ['./error.component.scss']
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.scss'],
 })
-
-
 export class ErrorComponent {
-    constructor(@Inject(MAT_DIALOG_DATA) public data: {message: string}) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { message: string }) {}
+
+  reloadPg(): void {
+    location.reload();
+  }
 }
