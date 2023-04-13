db7ac188d3d62fbaa67fc766e7983a0a326d16ab
Chase Olsen <skalolsen@gmail.com>
2023-04-12 23:34:00 -0600

    sweet

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2686,7 +2686,7 @@ router.get('/getInstructorsName', async (req, res) => {
  await Post.find({postLocationInstructor:
      {
         // problem is with '*'
-        $regex: new RegExp( payload + '*', 'i')
+        $regex: new RegExp('^' + payload , 'i')
  }
 })
  .then(instructorReviews => {
