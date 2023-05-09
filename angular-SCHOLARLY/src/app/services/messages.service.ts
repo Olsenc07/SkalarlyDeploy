c2d6c475ac619d7432440ca9135b7b6806983891
Chase Olsen <skalolsen@gmail.com>
2023-05-09 12:35:43 -0600

    hmmm

--- angular-SCHOLARLY/src/app/services/messages.service.ts
+++ angular-SCHOLARLY/src/app/services/messages.service.ts
@@ -53,8 +53,6 @@ export class MessageService {
       )
       .subscribe((transformedMessage) => {
         this.messages = transformedMessage;
-        this.messagesUpdated.next([]);
-        console.log('empty msg to clear');
         this.messagesUpdated.next([...this.messages]);
         console.log('msgs sent');
         sub.unsubscribe();
