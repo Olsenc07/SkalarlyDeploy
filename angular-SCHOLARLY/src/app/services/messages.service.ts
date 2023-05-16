90b486c1b112f8ad5bb0f1f7fee60fea8f303a70
Chase Olsen <skalolsen@gmail.com>
2023-05-16 15:53:53 -0600

    keep on saying

--- angular-SCHOLARLY/src/app/services/messages.service.ts
+++ angular-SCHOLARLY/src/app/services/messages.service.ts
@@ -32,57 +32,40 @@ export class MessageService {
   async autoFillAI(text: string): Promise<any> {
     console.log('autofill text', text);
 
-    const options = {
-      method: 'POST',
-      url: 'https://typewise-ai.p.rapidapi.com/completion/complete',
-      headers: {
-        'content-type': 'application/json',
-        'X-RapidAPI-Key': '3ac01901admsh99a9683c407b28ap1d39eajsnf9b1b4e2e51b',
-        'X-RapidAPI-Host': 'typewise-ai.p.rapidapi.com',
-      },
-      data: {
-        text: text,
-        correctTypoInPartialWord: false,
-        language: 'en',
-      },
-    };
-
-    try {
-      const response = await axios.request(options);
-      console.log('windy brains', response.data);
-    } catch (error) {
-      console.error(error);
-    }
-
     // Getapi key
-    // this.http
-    //   .get<{ message: string; messages: any }>(
-    //     'https://www.skalarly.com/api/messages/HiddenApiKey'
-    //   )
-    //   .subscribe((transformedMessage) => {
-    // console.log('gotron', transformedMessage.messages);
-    // const HiddenApiKey = transformedMessage.messages;
-    // const sub = this.http
-    //   .post('https://typewise-ai.p.rapidapi.com/completion/complete', {
-    //     headers: {
-    //       'content-type': 'application/json',
-    //       'X-RapidAPI-Key':
-    //         '3ac01901admsh99a9683c407b28ap1d39eajsnf9b1b4e2e51b',
-    //       'X-RapidAPI-Host': 'typewise-ai.p.rapidapi.com',
-    //     },
-    //     data: {
-    //       text: text,
-    //       correctTypoInPartialWord: false,
-    //       language: 'en',
-    //     },
-    //   })
-    //   .subscribe((transformedMessage) => {
-    //     console.log('lets see it all', transformedMessage);
-    //     this.messagesAutoFill.next(transformedMessage);
-    //     sub.unsubscribe();
-    //     console.log('eazy 1');
-    //   });
-    // });
+    this.http
+      .get<{ message: string; messages: any }>(
+        'https://www.skalarly.com/api/messages/HiddenApiKey'
+      )
+      .subscribe(async (transformedMessage) => {
+        console.log('gotron', transformedMessage.messages);
+        const HiddenApiKey = transformedMessage.messages;
+        const options = {
+          method: 'POST',
+          url: 'https://typewise-ai.p.rapidapi.com/completion/complete',
+          headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': HiddenApiKey,
+            'X-RapidAPI-Host': 'typewise-ai.p.rapidapi.com',
+          },
+          data: {
+            text: text,
+            correctTypoInPartialWord: false,
+            language: 'en',
+          },
+        };
+
+        try {
+          const response = await axios.request(options);
+          console.log('windy brains', response.data.predictions);
+          console.log('windy brains', response.data.predictions[0].text);
+          this.messagesAutoFill.next(response.data.predictions[0].text);
+        } catch (error) {
+          console.error(error);
+        }
+
+        console.log('eazy 1');
+      });
   }
 
   getInfoAutoFill(): any {
